b77bab21ad44c68760d3a4132545957a89151b13
carlahremarklund <carl.ahremark@lime.tech>
2021-04-03 10:39:16 +0200

    before adding vuetify

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,33 +1,39 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
+
 <template>
   <div class="container">
     <h1>{{ msg }}</h1>
-
+    <div class="board">
+      <v-row v-for="row in size" v-key="'r' + row">
+        <v-col v-for="column in size" v-key="'c'+column">
+          <Tile :row="row" :column="column" :key="`${row}${column}`"/>
+        </v-col>
+      </v-row>
+    </div>
   </div>
 </template>
 
 <script>
+import Tile from './Tile.vue';
+
 export default {
   name: 'Board',
   props: {
     msg: String,
   },
+
+  components: {
+    Tile,
+  },
+
+  data() {
+    return {
+      size: 4,
+    };
+  },
+  computed: {},
+  methods: {},
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/Tile.vue
@@ -0,0 +1,34 @@
+<style scoped>
+</style>
+
+<template>
+  <div class="container">
+    <span>
+      {{row }}, {{column}}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tile',
+  props: {
+    row: {
+      type: Number,
+      required: true,
+    },
+    column: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      size: 4,
+    };
+  },
+  computed: {},
+  methods: {},
+};
+</script>

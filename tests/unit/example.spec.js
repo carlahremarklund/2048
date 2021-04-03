d71dd9b0cccd9cd9bdacfd7875d7b286590e812d
carlahremarklund <carl.ahremark@lime.tech>
2021-04-03 10:00:37 +0200

    Fix: initial commit
    SEtup with router, store, empty board component

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import Board from '@/components/Board.vue';
 
-describe('HelloWorld.vue', () => {
+describe('Board.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Board, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
